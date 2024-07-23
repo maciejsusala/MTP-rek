d15dbcdaff165cfa6a964f20704f50cb38c3c922
maciejsusala <150375661+maciejsusala@users.noreply.github.com>
2024-07-23 18:11:24 +0200

    initial commit

---
+++ src/main/java/com/MTP/rek/RekApplication.java
@@ -0,0 +1,13 @@
+package com.MTP.rek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MTP/rek/RekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MTP.rek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
