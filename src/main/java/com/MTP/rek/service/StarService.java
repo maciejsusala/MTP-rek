9e6023e672cd3bad26d7f9074b342c192a8a0a37
maciejsusala <150375661+maciejsusala@users.noreply.github.com>
2024-07-24 12:12:00 +0200

    StarService v2, and tests

--- src/main/java/com/MTP/rek/service/StarService.java
+++ src/main/java/com/MTP/rek/service/StarService.java
@@ -9,29 +9,26 @@ import java.util.stream.Collectors;
 public class StarService {
 
     public List<Star> findClosestStars(List<Star> stars, int size) {
-        stars.sort(Comparator.comparingLong(Star::getDistance));
-        return new ArrayList<>(stars.subList(0, Math.min(size, stars.size())));
+        return stars.stream()
+                .sorted(Comparator.comparingLong(Star::getDistance))
+                .limit(size)
+                .collect(Collectors.toList());
     }
 
     public Map<Long, Integer> getNumberOfStarsByDistances(List<Star> stars) {
-        Map<Long, Integer> starsByDistance = new TreeMap<>();
-        for (Star star : stars) {
-            long distance = star.getDistance();
-            starsByDistance.put(distance, starsByDistance.getOrDefault(distance, 0) + 1);
-        }
-        return starsByDistance;
+        return stars.stream()
+                .collect(Collectors.groupingBy(Star::getDistance, Collectors.collectingAndThen(Collectors.counting(), Long::intValue)))
+                .entrySet().stream()
+                .sorted(Map.Entry.comparingByKey())
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
     }
 
     public Collection<Star> getUniqueStars(Collection<Star> stars) {
-        Map<String, Star> uniqueStars = new HashMap<>();
-        for (Star star : stars) {
-            if (!uniqueStars.containsKey(star.getName())) {
-                uniqueStars.put(star.getName(), star);
-            }
-        }
-        return uniqueStars.values();
+        return stars.stream()
+                .collect(Collectors.toMap(Star::getName, star -> star, (star1, star2) -> star1))
+                .values();
     }
-
-    //Uwagi - wiem że możnaby zastosować programowanie funkcyjne,
-    // ale nie jestem pewien czy to jest wymagane
 }
