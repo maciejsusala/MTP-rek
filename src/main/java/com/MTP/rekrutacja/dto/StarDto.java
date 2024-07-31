9ab1556d888a6386577d75ff45f174672ad79982
maciejsusala <150375661+maciejsusala@users.noreply.github.com>
2024-07-31 14:59:10 +0200

    Added validation for distance

--- src/main/java/com/MTP/rekrutacja/dto/StarDto.java
+++ src/main/java/com/MTP/rekrutacja/dto/StarDto.java
@@ -1,16 +1,18 @@
 package com.MTP.rekrutacja.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 
 import java.io.Serializable;
 
-public record StarDto (
+public record StarDto(
         Long id,
 
         @NotBlank(message = "Name cannot be blank")
         String name,
 
-        //TODO add validation for distance
+        @NotBlank(message = "Distance cannot be blank")
+        @Min(value = 0, message = "Distance must be greater than 0")
         long distance
 ) implements Serializable {
 }
